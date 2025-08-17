22880c4cd13aa793b9ee26c9d350a48c789749f9
Lbourgeois98 <73048129+Lbourgeois98@users.noreply.github.com>
2025-08-17 13:02:55 -0600

    Update agents.ts

--- src/data/agents.ts
+++ src/data/agents.ts
@@ -7,7 +7,6 @@ export const agents: Agent[] = [
     verificationId: 'SB-12345',
     status: 'verified',
     website: 'https://www.facebook.com/acey.luck?mibextid=ZbWKwL',
-    contactEmail: 'support@luckyace.com'
   },
   {
     id: '2',
@@ -15,15 +14,13 @@ export const agents: Agent[] = [
     verificationId: 'SB-23456',
     status: 'verified',
     website: 'https://m.me/j/AbbamX_9jW_xcJb6/',
-    contactEmail: 'support@tigersweeps.com'
   },
   {
     id: '3',
     name: 'Mega Money',
     verificationId: 'SB-34567',
     status: 'verified',
-    website: 'https://beacons.ai/megamoney',
-    contactEmail: 'support@megamoney.com'
+    website: 'https://megamony.carrd.co/',
   },
   {
     id: '5',
@@ -31,7 +28,6 @@ export const agents: Agent[] = [
     verificationId: 'SB-56789',
     status: 'verified',
     website: 'https://loungereloaded.com/sign-up?fbclid=IwZXh0bgNhZW0CMTEAAR0A5NWq8O7rvTgy46YiPr6Z18bGlo7tDqugqPEn2iAsvPYSh6dtdiaKMo4_aem_fih_FRwvFfm3KmsfZ6M9Hg',
-    contactEmail: 'support@thelounge.com'
   },
   {
     id: '6',
@@ -39,7 +35,6 @@ export const agents: Agent[] = [
     verificationId: 'SB-67890',
     status: 'verified',
     website: 'https://www.facebook.com/profile.php?id=61572466432737',
-    contactEmail: 'jake@bunnybliss.com'
   },
   {
     id: '7',
@@ -47,7 +42,6 @@ export const agents: Agent[] = [
     verificationId: 'SB-78901',
     status: 'verified',
     website: 'https://www.facebook.com/season.james?mibextid=ZbWKwL',
-    contactEmail: 'season.james@gaming.com'
   },
   {
     id: '9',
@@ -55,7 +49,6 @@ export const agents: Agent[] = [
     verificationId: 'SB-90123',
     status: 'verified',
     website: 'https://www.facebook.com/share/1FV8UYQeDA/',
-    contactEmail: 'nick@nicksweeps.com'
   },
   {
     id: '10',
@@ -63,15 +56,13 @@ export const agents: Agent[] = [
     verificationId: 'SB-01234',
     status: 'verified',
     website: 'https://www.facebook.com/profile.php?id=100093435218204',
-    contactEmail: 'harley@harleyluck.com'
   },
   {
     id: '11',
-    name: 'Snakes E Fountaine',
+    name: 'Underground Arcade',
     verificationId: 'SB-11111',
     status: 'verified',
-    website: 'https://www.facebook.com/snakes.fountaine?mibextid=ZbWKwL',
-    contactEmail: 'snakes@gaming.com'
+    website: 'https://www.facebook.com/profile.php?id=61562769076424',
   },
   {
     id: '12',
@@ -79,7 +70,6 @@ export const agents: Agent[] = [
     verificationId: 'SB-22222',
     status: 'verified',
     website: 'https://www.facebook.com/profile.php?id=100095035060766&mibextid=ZbWKwL',
-    contactEmail: 'support@kingsfisher.com'
   },
   {
     id: '13',
@@ -87,7 +77,6 @@ export const agents: Agent[] = [
     verificationId: 'SB-33333',
     status: 'verified',
     website: 'https://www.facebook.com/profile.php?id=61555972143744&mibextid=ZbWKwL',
-    contactEmail: 'shawn@gaming.com'
   },
   {
     id: '16',
@@ -95,7 +84,6 @@ export const agents: Agent[] = [
     verificationId: 'SB-55555',
     status: 'verified',
     website: 'https://www.facebook.com/erica.soriano.9?mibextid=ZbWKwL',
-    contactEmail: 'erica@gaming.com'
   },
   {
     id: '17',
@@ -103,7 +91,6 @@ export const agents: Agent[] = [
     verificationId: 'SB-66666',
     status: 'verified',
     website: 'https://www.facebook.com/luckygamingarena.1?mibextid=ZbWKwL',
-    contactEmail: 'support@luckygamingarena.com'
   },
   {
     id: '18',
@@ -111,6 +98,12 @@ export const agents: Agent[] = [
     verificationId: 'SB-77777',
     status: 'verified',
     website: 'https://dragon.club/',
-    contactEmail: 'support@dragon.club'
+  },
+  {
+    id: '19',
+    name: 'Spin City Treasures',
+    verificationId: 'SB-88888',
+    status: 'verified',
+    website: 'https://www.facebook.com/groups/619382011157848',
   }
 ];
